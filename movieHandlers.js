50232522e8d0429678820f285564eec071906c89
Cyril Freeman <cyril.freeman@hotmail.fr>
2023-05-08 17:29:09 +0200

    express quest 3 finish

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,65 +1,5 @@
 const database = require("./database");
 
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
-
-const users = [
-  {
-    id: 1,
-    firstname: "John",
-    lastname: "Doe",
-    mail: "john.doe@example.com",
-  },
-  {
-    id: 2,
-    firstname: "Jane",
-    lastname: "Doe",
-    mail: "jane.doe@example.com",
-  },
-  {
-    id: 3,
-    firstname: "Bob",
-    lastname: "Smith",
-    mail: "bob.smith@example.com",
-  },
-  {
-    id: 4,
-    firstname: "Alice",
-    lastname: "Jones",
-    mail: "alice.jones@example.com",
-  },
-  {
-    id: 5,
-    firstname: "James",
-    lastname: "Brown",
-    mail: "james.brown@example.com",
-  },
-];
-
 const getMovies = (req, res) => {
   database
     .query("select * from movies")
@@ -74,7 +14,6 @@ const getMovies = (req, res) => {
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
-
   database
     .query("select * from movies where id = ?", [id])
     .then(([movies]) => {
@@ -90,7 +29,25 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
--- usersHandlers.js
+++ usersHandlers.js
@@ -14,7 +14,6 @@ const getUsers = (req, res) => {
 
 const getUsersById = (req, res) => {
   const id = parseInt(req.params.id);
-
   database
     .query("select * from users where id = ?", [id])
     .then(([users]) => {
@@ -30,7 +29,25 @@ const getUsersById = (req, res) => {
     });
 };
 
+const postUsers = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
 module.exports = {
   getUsers,
   getUsersById,
+  postUsers,
 };
